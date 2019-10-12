f8a707b362e5821b4cdf7ecb497164d018483f7b
Michale Mao <790975625@qq.com>
2019-10-12 17:12:53 +0100

    Update

---
+++ ATA/src/test/java/ata/AObject.java
@@ -0,0 +1,40 @@
+package ata;
+
+import java.util.ArrayList;
+
+public class AObject {
+    private String ObjectName;
+    private int ObjectID;
+    private ArrayList<Picture> Pictures;
+    private ArrayList<Phrase> Phrases;
+    private ArrayList<Snippet> Snippets;
+
+    public AObject(String ObjectName, int ObjectID, ArrayList<Picture> Pictures, ArrayList<Phrase> Phrases, ArrayList<Snippet> Snippets){
+        super();
+        this.ObjectName = ObjectName;
+        this.ObjectID = ObjectID;
+        this.Pictures = Pictures;
+        this.Phrases = Phrases;
+        this.Snippets = Snippets;
+    }
+
+    public String getObjectName() {
+        return ObjectName;
+    }
+
+    public int getObjectID() {
+        return ObjectID;
+    }
+
+    public ArrayList<Phrase> getPhrases() {
+        return Phrases;
+    }
+
+    public ArrayList<Picture> getPictures() {
+        return Pictures;
+    }
+
+    public ArrayList<Snippet> getSnippets() {
+        return Snippets;
+    }
+}
